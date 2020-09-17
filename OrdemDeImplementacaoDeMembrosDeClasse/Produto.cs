69b99563dcf1031aeffdece52c5966034ffb98bd
David Moraes <investtraderrs@gmail.com>
2020-09-17 12:11:53 -0300

    Ordem de Implementação De Membros De Classe

---
+++ OrdemDeImplementacaoDeMembrosDeClasse/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        //Ordem Sugerida de Boas Praticas Para implementação de Membros de Classes:
+        //1º Atributos Privados
+        //2º Propriedades autoimplementadas
+        //3º Construtores
+        //4º Propriedades Customizadas
+        //5º Outros Métodos da Classe
+
+
+        private string _nome; //Exemplo de Atributos Privados
+        public double Preco { get; private set; } // Exemplo de Propriedades AutoImplementadas
+        public int Quantidade { get; private set; }
+
+       
+        public Produto() //Exemplo de Construtores
+        {
+        }
+        public Produto(string nome, double preco, int quantidade) //Exemplo de Construtores
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome //Exemplo de Propriedades Customizadas
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque //Exemplo do resto abaixo como Outros Métodos da Classe
+        {
+            get { return Preco * Quantidade; }
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrdemDeImplementacaoDeMembrosDeClasse/Program.cs
@@ -0,0 +1,18 @@
+﻿using Course;
+using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "TV 4k";
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
